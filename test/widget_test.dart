fa0dfca58a78bf3c699cc85418f282fe3559492a
elsa <230103033@mhs.udb.ac.id>
2025-07-21 14:00:38 +0700

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:new_lexclusive/main.dart';
+import 'package:flutter_supabase_notes_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
